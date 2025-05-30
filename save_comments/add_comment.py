e8fd5fa1b9150f00a6a9b83e21b582ec78e311e5
edwardpcharles <110316665+edwardpcharles@users.noreply.github.com>
2025-05-29 23:05:51 -0700

    Update add_comment.py

--- save_comments/add_comment.py
+++ save_comments/add_comment.py
@@ -1,53 +1,22 @@
 import fabric.functions as fn
-import requests
-from azure.identity import ClientSecretCredential
-import time
 
 udf = fn.UserDataFunctions()
 
+@udf.connection(argName="sqlDB",alias="sqldb")
 @udf.function()
-def write_to_excel(idVal: str, firstName:str, lastName:str) -> str:
-    TENANT_ID = ""
-    CLIENT_ID = ""
-    CLIENT_SECRET = ""
+def AddAnnotation(sqlDB: fn.FabricSqlConnection, commentdatetime: str, salespo: str, user: str, comment: str) -> str:
+    
+    data = (commentdatetime, user, salespo, comment,"1900-12-31")
+ 
+    connection = sqlDB.connect()
+    cursor = connection.cursor()    
 
-    credential = ClientSecretCredential(TENANT_ID, CLIENT_ID, CLIENT_SECRET)
-    access_token = credential.get_token("https://graph.microsoft.com/.default").token
-
-    site_id = ""
-    table_name = ""
-    excel_file_name = ""
-    url = f"https://graph.microsoft.com/v1.0/sites/{site_id}/drive/root:/{excel_file_name}.xlsx:/workbook/tables/{table_name}/rows"
-
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
-    }
-
-    payload = {
-        "values": [
-            [idVal, firstName, lastName]
-        ]
-    }
-
-    response = requests.post(url, headers=headers, json=payload)
-
-    credential = ClientSecretCredential(TENANT_ID, CLIENT_ID, CLIENT_SECRET)
-    access_token = credential.get_token("https://analysis.windows.net/powerbi/api/.default").token
-
-    group_id = ""
-    dataset_id = ""
-    refresh_url = f"https://api.powerbi.com/v1.0/myorg/groups/{group_id}/datasets/{dataset_id}/refreshes"
-
-    headers = {
-    "Authorization": f"Bearer {access_token}",
-    "Content-Type": "application/json"
-    }   
-
-    response = requests.post(refresh_url, headers=headers)
-    time.sleep(10)
-
-    if response.status_code == 201:
-        return "Row added successfully!"
-    else:
-        return f"Error: {response.status_code} - {response.text}"
+    if(salespo=="" or len(salespo) < 1):
+        raise fn.UserThrownError("Please Select a PO on the table to add a comment")
+ 
+    
+    insert_query = "INSERT INTO [dbo].[report_comments] ([commentdatetime],[user_id],[sales_po],[comment], [update_date_time]) VALUES (?, ?, ?, ?, ?);"
+    cursor.execute(insert_query, data)
+    connection.commit()
+    connection.close()               
+    return "Comment was successfully added"
